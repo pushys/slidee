e27993fe9e06715a7f5e9791c00f84976b4f858b
pushys <evgeny.dknv@gmail.com>
2026-08-23 02:11:37 +0300

    Fail oxlint/oxfmt only in test environment

--- vite.config.ts
+++ vite.config.ts
@@ -8,50 +8,53 @@ import oxlintPlugin from 'vite-plugin-oxlint';
 import { defineConfig } from 'vitest/config';
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
-export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({
-      presets: [reactCompilerPreset()],
-    }),
-    oxlintPlugin({ failOnError: true, failOnWarning: true }),
-  ],
-  resolve: {
-    tsconfigPaths: true,
-  },
-  test: {
-    projects: [
-      {
-        extends: true,
-        test: {
-          environment: 'happy-dom',
-        },
-      },
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(import.meta.dirname, '.storybook'),
-          }),
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [
-              {
-                browser: 'chromium',
-              },
-            ],
+export default defineConfig(({ mode }) => {
+  const isTest = mode === 'test';
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      babel({
+        presets: [reactCompilerPreset()],
+      }),
+      oxlintPlugin({ failOnError: isTest, failOnWarning: isTest }),
+    ],
+    resolve: {
+      tsconfigPaths: true,
+    },
+    test: {
+      projects: [
+        {
+          extends: true,
+          test: {
+            environment: 'happy-dom',
           },
         },
-      },
-    ],
-  },
+        {
+          extends: true,
+          plugins: [
+            // The plugin will run tests for the stories defined in your Storybook config
+            // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+            storybookTest({
+              configDir: path.join(import.meta.dirname, '.storybook'),
+            }),
+          ],
+          test: {
+            name: 'storybook',
+            browser: {
+              enabled: true,
+              headless: true,
+              provider: playwright({}),
+              instances: [
+                {
+                  browser: 'chromium',
+                },
+              ],
+            },
+          },
+        },
+      ],
+    },
+  };
 });
