05a1c3e49cf99f3cf8858e9adeb62f2b84277f02
pushys <evgeny.dknv@gmail.com>
2026-09-02 18:08:37 +0300

    `I18nextProvider` decorator

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,13 @@
 import type { Preview } from '@storybook/react-vite';
 
-import '@fontsource/inter';
+import { Spinner } from '@heroui/react';
+import { Suspense } from 'react';
+import { I18nextProvider } from 'react-i18next';
 
 import '../src/index.css';
+import i18n from '../src/app/i18n';
+
+import '@fontsource/inter';
 
 const preview: Preview = {
   parameters: {
@@ -20,6 +25,13 @@ const preview: Preview = {
       document.body.classList.add('bg-background', 'text-foreground');
       return <Story />;
     },
+    (Story) => (
+      <Suspense fallback={<Spinner />}>
+        <I18nextProvider i18n={i18n}>
+          <Story />
+        </I18nextProvider>
+      </Suspense>
+    ),
   ],
   argTypes: {
     className: { table: { disable: true } },
